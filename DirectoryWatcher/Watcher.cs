57d2bf0c0312093b31827d567f5e4e20aa31773a
alipala <ali.pala@ymail.com>
2019-05-20 16:38:19 +0300

    fix for starting copy operation and put into watch method

--- DirectoryWatcher/Watcher.cs
+++ DirectoryWatcher/Watcher.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Permissions;
+using System.Threading;
 
 namespace DirectoryWatcher
 {
@@ -10,64 +10,51 @@ namespace DirectoryWatcher
         static List<string> dirList = new List<string>();
         static void Main()
         {
-            //Start file watch operaion
+            //Start the file watch operaion
             Watch();
-            //dirList[0] is the root level of the directory
-            copyOperation(dirList[0]);
-
-
-
         }
-
-
-        
 
 
         private static void Watch()
         {
-            string directory = @"\\192.168.4.95\share\4Leon";
+            string directory = @"\\ad001.siemens.net\dfs001\File\CI_GEN\Prototypen_IT\S7-CPs\Ind-Ethernet\CC7-CloudConnect\cc7";
 
             using (FileSystemWatcher directoryWatcher = new FileSystemWatcher())
             {
                 directoryWatcher.Path = directory;
-
-                //directoryWatcher.NotifyFilter = NotifyFilters.LastAccess
-                //                              | NotifyFilters.LastWrite
-                //                              | NotifyFilters.FileName
-                //                              | NotifyFilters.DirectoryName;
-
-                //directoryWatcher.Filter = "*.pdf";
-
+            
                 directoryWatcher.Created += watchChanged;
 
                 directoryWatcher.EnableRaisingEvents = true;
                 directoryWatcher.IncludeSubdirectories = true;
 
-
-
-
-
-                Console.WriteLine("Press 'q' to quit the sample. ");
-                while (Console.Read() != 'q')
-                    ;
+                Console.WriteLine("Press 'q' to quit. ");
+                while (Console.Read() != 'q');
 
             }
         }
 
-        private static void copyOperation(string source, string destDir = @"\\192.168.4.95\share\paste")
+        /// <summary>
+        /// If any change occurs in the source,
+        /// It starts to copy the file into destDir
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destDir"></param>
+
+        private static void copyOperation(string source, string destDir = @"\\192.168.0.251\share\paste")
         {
             Console.WriteLine("Search files in the folder...");
-            string[] files = Directory.GetFiles(source, "*.jpg", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(source, "*.upd", SearchOption.AllDirectories);
 
             foreach (var file in files)
             {
                 // Extract only the file name from the path.
                 string fileName = Path.GetFileName(file);
-                string  destFile = Path.Combine(destDir, fileName);
+                string destFile = Path.Combine(destDir, fileName);
 
                 Console.WriteLine("Copying is started...");
                 File.Copy(file, destFile, true);
-            
+
                 Console.WriteLine(file);
             }
 
@@ -78,6 +65,8 @@ namespace DirectoryWatcher
         {
             Console.WriteLine($"File Path: {e.FullPath} and Change Type: {e.ChangeType}");
             dirList.Add(e.FullPath);
+            Thread.Sleep(500);
+            copyOperation(dirList[0]);
 
         }
 
